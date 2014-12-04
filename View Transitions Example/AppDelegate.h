91563c1452d065b594170e5d271fd7d15b33ef92
xeaza <taylorwrightsanson@gmail.com>
2014-12-04 13:08:02 -0500

    Initial Commit

---
+++ View Transitions Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  View Transitions Example
+//
+//  Created by Taylor Wright-Sanson on 12/4/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ View Transitions ExampleTests/View_Transitions_ExampleTests.m
@@ -0,0 +1,40 @@
+//
+//  View_Transitions_ExampleTests.m
+//  View Transitions ExampleTests
+//
+//  Created by Taylor Wright-Sanson on 12/4/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface View_Transitions_ExampleTests : XCTestCase
+
+@end
+
+@implementation View_Transitions_ExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
